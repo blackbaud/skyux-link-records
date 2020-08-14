025cbd03f5fc9c8e414d74ffa89cce3950625c6a
Steve Brush <steve.brush@blackbaud.com>
2020-08-14 10:25:03 -0400

    Install `@skyux/docs-tools` (#33)

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -3,6 +3,11 @@ import {
 } from '@angular/core';
 
 import {
+  SkyDocsToolsModule,
+  SkyDocsToolsOptions
+} from '@skyux/docs-tools';
+
+import {
   SkyAppLinkModule
 } from '@skyux/router';
 
@@ -13,7 +18,17 @@ import {
 @NgModule({
   exports: [
     SkyAppLinkModule,
+    SkyDocsToolsModule,
     SkyLinkRecordsModule
+  ],
+  providers: [
+    {
+      provide: SkyDocsToolsOptions,
+      useValue: {
+        gitRepoUrl: 'https://github.com/blackbaud/skyux-link-records',
+        packageName: '@skyux/link-records'
+      }
+    }
   ]
 })
 export class AppExtrasModule { }
