39455ca615dfa08447d3985d32e0c4503c67f22c
Denise Pena <denise.pena@blackbaud.me>
2020-09-29 15:53:24 -0500

    Added Design tab content

---
+++ src/app/docs/link-records/link-records-docs.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-link-records-docs',
+  templateUrl: './link-records-docs.component.html'
+})
+export class LinkRecordsDocsComponent { }
